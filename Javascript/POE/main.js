38f2609827cc1643483e2c02bdd49b299b980f53
auroradewebs-ops <auroradewebs@gmail.com>
2026-01-16 16:07:31 -0400

    poe cositas

--- Javascript/POE/main.js
+++ Javascript/POE/main.js
@@ -1,16 +1,39 @@
-// const label = document.querySelector('label');
-// label.textContent = 'Adios';
+// // const label = document.querySelector('label');
+// // label.textContent = 'Adios';
 
-// const labels = document.querySelectorAll('label');
-// for(const label of labels){
-//     label.textContent = 'Adios';
-// }
+// // const labels = document.querySelectorAll('label');
+// // for(const label of labels){
+// //     label.textContent = 'Adios';
+// // }
 
-// const btnTouchMe = document.getElementById('btnTouchMe');
-// btnTouchMe.textContent = 'Hola';
+// // const btnTouchMe = document.getElementById('btnTouchMe');
+// // btnTouchMe.textContent = 'Hola';
 
-// const cosita = document.getElementsByClassName('cosita');
-// cosita[0].textContent = 'Hey!';
+// // const cosita = document.getElementsByClassName('cosita');
+// // cosita[0].textContent = 'Hey!';
 
-const label = document.querySelector('label[name="trunks"]');
-label.textContent = 'Trunks';
+// const label = document.querySelector('label[name="trunks"]');
+// label.textContent = 'Trunks';
+
+
+
+
+// Nueva clase
+
+const title = document.getElementById('title'); //Esto es lo mismo que el querySelector('title');
+const changeTextBtn = document.getElementById('changeTextBtn');
+const changeColorBtn = document.getElementById('changeColorBtn');
+const addItemBtn = document.getElementById('addItemBtn');
+const toggleClassBtn = document.getElementById('toggleClassBtn');
+const message = document.getElementById('message');
+const itemList = document.getElementById('itemList');
+
+title.textContent = 'Aqui cambia title';
+
+/* Events listeners 
+
+addEventListener(eventName, callback);
+
+retomar clase en min 17:52. Leccion 13
+
+*/
